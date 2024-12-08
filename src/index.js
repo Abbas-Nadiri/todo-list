dd8311dacb64327a6ee7e6fb5af861ee1e0f1357
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-12-08 14:05:47 +0000

    create displayToDoItem function to display to-do items on the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+import ToDoItem from "./todo-class";
+import Project, { projectsArray, createProject, removeProject , moveTask, createToDo} from "./project-class";
 import "./styles.css";
+
+export let currentProject = projectsArray.getProject("Default");
+
+createToDo("test","DO THE THING", "ASAP", "TURBO HIGH");
+createToDo("alternate", "wowie", "hehehe", "kind of");
+createToDo("kreyzi", "when", "maybe way", "mistake");
+
+createProject("test project 1");
+currentProject = projectsArray.getProject("test project 1");
+
+createToDo("shaboing", "what", "no way", "tragedy");
+createToDo("blabla", "where", "yes way", "disoster");
+
+const tasksDisplay = document.querySelector(".tasks-display");
+
+//get each toDoItem in currentProject and display them as taskCards
+function displayToDoItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("filler");
+
+    const taskCardTitle = document.createElement("div");
+    const taskCardDue = document.createElement("div");
+
+    taskCardTitle.textContent = item.title;
+    taskCardDue.textContent = item.dueDate;
+
+    div.append(taskCardTitle);
+    div.append(taskCardDue);
+    tasksDisplay.append(div);
+}
+
+currentProject.tasks.forEach(item => displayToDoItem(item));
+
+//update the pending display number
+const pendingCount = document.querySelector(".pending-count");
+pendingCount.textContent = document.querySelector(".tasks-display").childElementCount;
