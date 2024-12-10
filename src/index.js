5c12f03d546784ded586733288368bf0d28f5e74
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-12-10 07:31:32 +0000

    style task cards and begin implementing complete/incomplete status for each task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ToDoItem from "./todo-class";
 import Project, { projectsArray, createProject, removeProject , moveTask, createToDo} from "./project-class";
 import "./styles.css";
+import untickedImg from "./icons/unticked.svg";
+import tickedImg from "./icons/ticked.svg";
+
 
 export let currentProject = projectsArray.getProject("Default");
 
@@ -18,20 +21,43 @@ const tasksDisplay = document.querySelector(".tasks-display");
 
 //get each toDoItem in currentProject and display them as taskCards
 function displayToDoItem(item) {
-    const div = document.createElement("div");
-    div.classList.add("filler");
+    const taskCard = document.createElement("div");
+    taskCard.classList.add("task-card");
 
+    const taskCardText = document.createElement("div");
+    taskCardText.classList.add("taskCard-text");
     const taskCardTitle = document.createElement("div");
     const taskCardDue = document.createElement("div");
-
     taskCardTitle.textContent = item.title;
     taskCardDue.textContent = item.dueDate;
 
-    div.append(taskCardTitle);
-    div.append(taskCardDue);
-    tasksDisplay.append(div);
-}
+    const taskCardButton = document.createElement("button");
+    const unticked = document.createElement("img");
+    const ticked = document.createElement("img");
+    unticked.src = untickedImg;
+    ticked.src = tickedImg;
+    taskCardButton.append(unticked);
 
+    // add ".completed" class to completed tasks 
+    
+    taskCardButton.addEventListener("click", () => {
+        taskCard.classList.toggle("completed-task");
+        console.log(taskCard.classList.contains("completed-task"));
+        if (taskCard.classList.contains("completed-task")) {
+            taskCardButton.innerHTML = "";
+            taskCardButton.append(ticked);
+        } else {
+            taskCardButton.innerHTML = "";
+            taskCardButton.append(unticked);
+        }
+    })
+
+    taskCardText.append(taskCardTitle);
+    taskCardText.append(taskCardDue);
+    taskCard.append(taskCardButton, taskCardText);
+    tasksDisplay.append(taskCard);
+}
+//do this for each project 
 currentProject.tasks.forEach(item => displayToDoItem(item));
 
 //update the pending display number
